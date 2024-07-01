2fc27e425a0085338719b7db6f402fd6eac7c2f3
MattiaBelfiore <160751662+MattiaBelfiore@users.noreply.github.com>
2024-07-01 10:02:34 +0200

    Delete TIW134 directory

--- TIW134/src/main/java/it/polimi/tiw/Project/controllers/DocumentManager.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.tiw.Project.controllers;
-
-public class DocumentManager {
-	
-}
--- TIW134/src/main/java/it/polimi/tiw/Project/dao/DocsDAO.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.tiw.Project.dao;
-
-public class DocsDAO {
-
-	
-	//metodo per aggiungere documenti ad una cartella
-	
-	//metodo per ottenere tutti i documenti in una cartella
-}
